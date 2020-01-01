b3c650d6d6568fc07397c41d99ef83f58fb8d52b
ykoba84 <yosuke@epx.phys.tohoku.ac.jp>
2020-01-01 16:21:40 +0900

    modified

--- nn.py
+++ nn.py
@@ -102,10 +102,3 @@ otp.CsvOutput_0238(combinations, outputpath)
 otp.TotalTimeOutput(total_time, outputpath)
 otp.HyperParameterOutput(batch_size, outputpath)
 gpt.loss_and_acc(hist, epochs, outputpath)
-
-sleeping_time = input("How long do you sleep?(hour) : ")
-sleeping_time = int(sleeping_time)
-if sleeping_time > 5:
-    print("😃 < good!")
-else:
-    print("🤒 < bad...")
